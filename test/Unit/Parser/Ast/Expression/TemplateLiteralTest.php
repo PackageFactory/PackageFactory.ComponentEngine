de79d2516d0e17c607eac210e8dfd5399980e688
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2020-07-05 16:57:34 +0200

    Improve line coverage for Ast/Expression/TemplateLiteral

---
+++ test/Unit/Parser/Ast/Expression/TemplateLiteralTest.php
@@ -0,0 +1,67 @@
+<?php declare(strict_types=1);
+namespace PackageFactory\ComponentEngine\Test\Unit\Parser\Ast\Expression;
+
+use PackageFactory\ComponentEngine\Parser\Ast\Expression\TemplateLiteral;
+use PackageFactory\ComponentEngine\Parser\Lexer\Tokenizer;
+use PackageFactory\ComponentEngine\Parser\Lexer\TokenStream;
+use PackageFactory\ComponentEngine\Parser\Lexer\Scope;
+use PackageFactory\ComponentEngine\Parser\Source\Source;
+use PHPUnit\Framework\TestCase;
+
+final class TemplateLiteralTest extends TestCase
+{
+    /**
+     * @return array<string, array{string, string, array<mixed>}>
+     */
+    public function provider(): array 
+    {
+        return [
+            'simple' => [
+                '`Hello World!`',
+                [
+                    'type' => 'TemplateLiteral',
+                    'offset' => [0, 13],
+                    'segments' => ['Hello World!']
+                ]
+            ],
+            'with escapes' => [
+                '`Hello \`World\``',
+                [
+                    'type' => 'TemplateLiteral',
+                    'offset' => [0, 16],
+                    'segments' => ['Hello `World`']
+                ]
+            ],
+            'exit after delimiter' => [
+                '`Hello World` ',
+                [
+                    'type' => 'TemplateLiteral',
+                    'offset' => [0, 12],
+                    'segments' => ['Hello World']
+                ]
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider provider
+     * @test
+     * @small
+     * @param string $input
+     * @param array<mixed> $asJson
+     * @return void
+     */
+    public function test(string $input, array $asJson): void
+    {
+        $source = Source::fromString($input);
+        $tokenizer = Tokenizer::fromSource($source, Scope\TemplateLiteral::class);
+        $stream = TokenStream::fromTokenizer($tokenizer);
+
+        $result = TemplateLiteral::fromTokenStream($stream);
+
+        $this->assertJsonStringEqualsJsonString(
+            (string) json_encode($asJson),
+            (string) json_encode($result)
+        );
+    }
+}
