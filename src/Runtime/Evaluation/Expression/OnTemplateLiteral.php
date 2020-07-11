385d5b1cbafe964d0144ebaa566d8b0c0c1277b3
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2020-07-11 21:39:01 +0200

    Implement TemplateLiteral evaluation

--- src/Runtime/Evaluation/Expression/OnTemplateLiteral.php
+++ src/Runtime/Evaluation/Expression/OnTemplateLiteral.php
@@ -13,7 +13,40 @@ final class OnTemplateLiteral
      */
     public static function evaluate(Runtime $runtime, TemplateLiteral $templateLiteral): string 
     {
-        throw new \Exception('@TODO: onTemplateLiteral');
+        $result = '';
+
+        foreach ($templateLiteral->getSegments() as $segment) {
+            if (is_string($segment)) {
+                $result .= $segment;
+            } else {
+                $value = OnTerm::evaluate($runtime, $segment);
+
+                if (is_string($value)) {
+                    $result .= $value;
+                } elseif (is_numeric($value)) {
+                    $valueAsInt = intval($value);
+                    if ($valueAsInt == $value) {
+                        $result .= $valueAsInt;
+                    } else {
+                        $result .= $value;
+                    }
+                } elseif (is_array($value)) {
+                    $valueFlattened = [];
+                    array_walk_recursive($value, function($item) use (&$valueFlattened) { 
+                        $valueFlattened[] = $item; 
+                    });
+                    $result .= join(',', $valueFlattened);
+                } elseif (is_bool($value)) {
+                    $result .= $value ? 'true' : 'false';
+                } elseif ($value === null) {
+                    $result .= 'null';
+                } else {
+                    throw new \RuntimeException('@TODO: Failed string Conversion: ' . gettype($value));
+                }
+            }
+        }
+
+        return $result;
     }
 }
 
---
+++ test/Unit/Runtime/Evaluation/Expression/TemplateLiteralTest.php
@@ -0,0 +1,89 @@
+<?php declare(strict_types=1);
+namespace PackageFactory\ComponentEngine\Test\Unit\Runtime\Evaluation\Expression;
+
+use PackageFactory\ComponentEngine\Parser\Ast\Expression\TemplateLiteral;
+use PackageFactory\ComponentEngine\Parser\ExpressionParser;
+use PackageFactory\ComponentEngine\Parser\Lexer\Tokenizer;
+use PackageFactory\ComponentEngine\Parser\Lexer\TokenStream;
+use PackageFactory\ComponentEngine\Parser\Lexer\Scope;
+use PackageFactory\ComponentEngine\Parser\Source\Source;
+use PackageFactory\ComponentEngine\Runtime\Context;
+use PackageFactory\ComponentEngine\Runtime\Evaluation\Expression\OnTerm;
+use PackageFactory\ComponentEngine\Runtime\Runtime;
+use PHPUnit\Framework\TestCase;
+
+final class TemplateLiteralTest extends TestCase
+{
+    /**
+     * @return \Iterator<string, array{string, Context, mixed}>
+     */
+    public function averageCaseProvider(): \Iterator
+    {
+        $input = '`Hello, ${name}!`';
+        $context = Context::fromArray(['name' => 'Jane Doe']);
+        $result = "Hello, Jane Doe!";
+        yield $input => [$input, $context, $result];
+
+        $input = '`Point: ${x}, ${y}`';
+        $context = Context::fromArray(['x' => 12.0, 'y' => 25.3]);
+        $result = "Point: 12, 25.3";
+        yield $input => [$input, $context, $result];
+
+        $input = '`List: ${list}`';
+        $context = Context::fromArray(['list' => [1, 2, 3, 4]]);
+        $result = "List: 1,2,3,4";
+        yield $input => [$input, $context, $result];
+
+        $input = '`Nested List: ${list}`';
+        $context = Context::fromArray(['list' => [1, [2, 3], 4]]);
+        $result = "Nested List: 1,2,3,4";
+        yield $input => [$input, $context, $result];
+
+        $input = '`Boolean (true): ${boolean}`';
+        $context = Context::fromArray(['boolean' => true]);
+        $result = "Boolean (true): true";
+        yield $input => [$input, $context, $result];
+
+        $input = '`Boolean (false): ${boolean}`';
+        $context = Context::fromArray(['boolean' => false]);
+        $result = "Boolean (false): false";
+        yield $input => [$input, $context, $result];
+
+        $input = '`Null: ${null}`';
+        $context = Context::createEmpty();
+        $result = "Null: null";
+        yield $input => [$input, $context, $result];
+
+        // @TODO: This is not possible yet!
+        // $input = '`Hello, ${() => 15}!`';
+        // $context = Context::createEmpty();
+        // $result = "Hello, () => 15!";
+        // yield $input => [$input, $context, $result];
+    }
+
+    /**
+     * @test
+     * @small
+     * @dataProvider averageCaseProvider
+     * @param string $input
+     * @param Context $context
+     * @param mixed $value
+     * @return void
+     */
+    public function testAverageCase(string $input, Context $context, $value): void
+    {
+        $source = Source::fromString($input);
+        $tokenizer = Tokenizer::fromSource($source, Scope\Expression::class);
+        $stream = TokenStream::fromTokenizer($tokenizer);
+        
+        /** @var TemplateLiteral $ast */
+        $ast = ExpressionParser::parse($stream);
+
+        $result = OnTerm::evaluate(
+            Runtime::default()->withContext($context),
+            $ast
+        );
+
+        $this->assertSame($value, $result);
+    }
+}
