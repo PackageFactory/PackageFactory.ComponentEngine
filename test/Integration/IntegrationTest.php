d382a84444fb9609e2f523c0fff19f7221b200ae
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2022-04-10 15:46:40 +0200

    WIP: Add new test fixtures

---
+++ test/Integration/IntegrationTest.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * PackageFactory.ComponentEngine - Universal View Components for PHP
+ *   Copyright (C) 2022 Contributors of PackageFactory.ComponentEngine
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace PackageFactory\ComponentEngine\Test\Integration;
+
+use PackageFactory\ComponentEngine\Parser\Tokenizer\Tokenizer;
+use PackageFactory\ComponentEngine\Parser\Source\Source;
+use PHPUnit\Framework\TestCase;
+
+final class IntegrationTest extends TestCase
+{
+    public function exampleProvider(): array
+    {
+        return [
+            'Comment' => ["Comment"],
+            'Enum' => ["Enum"],
+            'Interface' => ["Interface"],
+            'Component' => ["Component"],
+            'Expression' => ["Expression"],
+        ];
+    }
+
+    /**
+     * @dataProvider exampleProvider
+     * @test
+     * @small
+     * @param string $input
+     * @param array<mixed> $asJson
+     * @return void
+     */
+    public function testTokenization(string $example): void
+    {
+        $source = Source::fromFile(__DIR__ . '/Examples/' . $example . '/' . $example . '.afx');
+        $tokenizer = Tokenizer::fromSource($source, Scope\Expression::class);
+        $expected = json_decode(
+            file_get_contents(__DIR__ . '/Examples/' . $example . '/' . $example . '.tokens.json')
+        );
+
+        $index = 0;
+        foreach ($tokenizer as $token) {
+            $this->assertEquals($expected[$index]->type, $token->type->name, "Type mismatch at index $index");
+            $this->assertEquals($expected[$index]->value, $token->value, "Value mismatch at index $index");
+            $index++;
+        }
+    }
+}
