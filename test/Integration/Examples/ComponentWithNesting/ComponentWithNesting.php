73bb23e6f110c1a42f03056ea77f6e9eac8cc5ac
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2022-11-02 16:53:35 +0100

    Update ComponentWithNesting example

--- test/Integration/Examples/ComponentWithNesting/ComponentWithNesting.php
+++ test/Integration/Examples/ComponentWithNesting/ComponentWithNesting.php
@@ -5,8 +5,6 @@ declare(strict_types=1);
 namespace Vendor\Project\Component;
 
 use Vendor\Project\BaseClass;
-use Vendor\Project\Std;
-use Vendor\Project\Hyperscript;
 
 final class TextWithImage extends BaseClass
 {
@@ -20,35 +18,11 @@ final class TextWithImage extends BaseClass
 
     public function render(): string
     {
-        return Hyperscript::tag(
-            'div',
-            Hyperscript::attributes(
-                Hyperscript::attribute('class', 'text-with-image'),
-                Hyperscript::attribute('src', Std::string($this->src)),
-                Hyperscript::attribute('alt', Std::string($this->alt)),
-                Hyperscript::attribute('title', Std::string($this->title))
-            ),
-            Hyperscript::children(
-                Hyperscript::tag(
-                    'img',
-                    Hyperscript::attributes(
-                        Hyperscript::attribute('class', 'image'),
-                        Hyperscript::attribute('src', Std::string($this->src)),
-                        Hyperscript::attribute('alt', Std::string($this->alt)),
-                        Hyperscript::attribute('title', Std::string($this->title))
-                    ),
-                    Hyperscript::children()
-                ),
-                Hyperscript::tag(
-                    'p',
-                    Hyperscript::attributes(
-                        Hyperscript::attribute('class', 'text')
-                    ),
-                    Hyperscript::children(
-                        Std::string($this->text)
-                    )
-                )
-            )
-        );
+        return '<div class="text-with-image">'
+            . '<img class="image" src={src} alt={alt} title={title} />'
+            . '<p class="text">'
+            . $this->text
+            . '</p>'
+            . '</div>';
     }
 }
