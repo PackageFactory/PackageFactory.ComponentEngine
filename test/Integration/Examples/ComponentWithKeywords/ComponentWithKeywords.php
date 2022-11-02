bc389732af07fe99d2818566bfd13347b291e760
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2022-11-02 16:50:17 +0100

    Update ComponentWithKeywords example

--- test/Integration/Examples/ComponentWithKeywords/ComponentWithKeywords.php
+++ test/Integration/Examples/ComponentWithKeywords/ComponentWithKeywords.php
@@ -5,18 +5,14 @@ declare(strict_types=1);
 namespace Vendor\Project\Component;
 
 use Vendor\Project\BaseClass;
-use Vendor\Project\Hyperscript;
 
 final class ComponentWithKeywords extends BaseClass
 {
     public function render(): string
     {
-        return Hyperscript::tag(
-            'div',
-            Hyperscript::attributes(),
-            Hyperscript::children(
-                Hyperscript::text('Keywords like import or export or component are allowed in here.')
-            )
-        );
+        return '<div>'
+            . 'Keywords like import or export or component'
+            . ' are allowed in here.'
+            . '</div>';
     }
 }
