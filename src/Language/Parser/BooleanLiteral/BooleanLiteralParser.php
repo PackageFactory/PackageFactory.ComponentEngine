1406f9ee3ffce47eba200883d4ec15d9c26caf35
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2023-07-25 13:51:37 +0200

    TASK: Implement `BooleanLiteralParser`

---
+++ src/Language/Parser/BooleanLiteral/BooleanLiteralParser.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * PackageFactory.ComponentEngine - Universal View Components for PHP
+ *   Copyright (C) 2023 Contributors of PackageFactory.ComponentEngine
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace PackageFactory\ComponentEngine\Language\Parser\BooleanLiteral;
+
+use PackageFactory\ComponentEngine\Language\AST\BooleanLiteral\BooleanLiteralNode;
+use PackageFactory\ComponentEngine\Language\Shared\Location\Location;
+use PackageFactory\ComponentEngine\Parser\Tokenizer\Scanner;
+use PackageFactory\ComponentEngine\Parser\Tokenizer\Token;
+use PackageFactory\ComponentEngine\Parser\Tokenizer\TokenType;
+
+final class BooleanLiteralParser
+{
+    /**
+     * @param \Iterator<mixed,Token> $tokens
+     * @return BooleanLiteralNode
+     */
+    public function parse(\Iterator $tokens): BooleanLiteralNode
+    {
+        Scanner::assertType($tokens, TokenType::KEYWORD_TRUE, TokenType::KEYWORD_FALSE);
+
+        $token = $tokens->current();
+        $value = $token->type === TokenType::KEYWORD_TRUE;
+
+        Scanner::skipOne($tokens);
+
+        return new BooleanLiteralNode(
+            location: new Location(
+                sourcePath: $token->sourcePath,
+                boundaries: $token->boundaries
+            ),
+            value: $value
+        );
+    }
+}
